2a23e15bb2fe162721eea86339677ad175fd375b
wangrun <yeesunday@gmail.com>
2015-10-28 21:56:25 +0800

    update frontend

---
+++ js/index.js
@@ -0,0 +1,71 @@
+$(function () {
+  var wrapperH = $('.wrapper').height();
+  var $code = $('.code');
+  var uid = 1;
+
+  $code.css({top: (wrapperH - 84 - 30) + 'px'});
+  getUid();
+
+  function connectWebViewJavascriptBridge (callback) {
+    if (window.WebViewJavascriptBridge) {
+      callback(WebViewJavascriptBridge)
+    } else {
+      document.addEventListener('WebViewJavascriptBridgeReady', function() {
+        callback(WebViewJavascriptBridge)
+      }, false)
+    }
+  }
+  function getUid () {
+    if (android) {
+      uid = window.web && web.getUserId().uid;
+      checkCode();
+    } else if (ios) {
+      connectWebViewJavascriptBridge(function (bridge) {
+        bridge.callHandler('getUserId', {}, function(response) {
+          alert(response);
+          uid = (JSON.parse(response)).uid;
+          alert(uid);
+          checkCode();
+        })
+      });
+    }
+  }
+  function checkCode () {
+    $.ajax({
+      method:'get',
+      url:'/backend/check_exist.php',
+      data: {uid: uid}
+    }).done(function (res) {
+      res = JSON.parse(res);
+      console.log(res.code);
+      if (res.code != 200) {
+        $('.modal').show();
+      }
+    });
+  }
+
+  $('.btn-submit').click(function () {
+    var code = $('.modal-input').val();
+
+    if (code != '') {
+      $.ajax({
+        method:'get',
+        url:'/backend/submit_code.php',
+        data: {
+          uid: uid,
+          code: code
+        }
+      }).done(function (res) {
+        console.log(res);
+        if (res.code != 200) {
+          $('.form-part').hide();
+          $('.tip-part').show().fadeOut(4000, function() {
+            $('.modal').hide();
+          });
+        }
+      });
+    } else {
+      alert('请填写兑换码哦~');
+    }
+  });
+})
